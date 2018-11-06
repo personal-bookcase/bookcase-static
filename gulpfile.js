efb54bf575a5a88b5d7bd35079d0f5528e666979
Alenushka <tsykunova.alena@yandex.ru>
2018-11-07 00:06:17 +0300

    add header, main and footer

--- gulpfile.js
+++ gulpfile.js
@@ -23,9 +23,19 @@ gulp.task("assets", function() {
                .pipe(gulp.dest("dist/assets"));
 });
 
+gulp.task("fonts", function() {
+    return gulp.src("src/fonts/**")
+               .pipe(gulp.dest("dist/fonts"));
+});
+ 
+gulp.task("js", function() {
+    return gulp.src("node_modules/@fortawesome/fontawesome-free/js/all.min.js")
+               .pipe(gulp.dest("dist/js"));
+});
+
 gulp.task("watch", function() {
     gulp.watch("src/**/*.html", gulp.series("html"));
-    gulp.watch("src/styles/style.less", gulp.series("styles"));
+    gulp.watch("src/styles/**/*.less", gulp.series("styles"));
     gulp.watch("src/assets/**", gulp.series("assets"));
 });
 
@@ -37,4 +47,4 @@ gulp.task("serve", function() {
     browserSync.watch("dist/**/*.*").on("change", browserSync.reload);
 });
 
-gulp.task("dev", gulp.series("html", "styles", "assets", gulp.parallel("watch", "serve")));
+gulp.task("dev", gulp.series("html", "styles", "assets", "fonts", "js", gulp.parallel("watch", "serve"))); 
